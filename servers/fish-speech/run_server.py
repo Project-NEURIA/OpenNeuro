f58117da4daf03455fefd9820d3d010527061325
Barry <barryy.jiang@mail.utoronto.ca>
2026-03-16 10:14:46 -0400

    change the CACHE_DIR in run_server i forgot I was using a strange local path

--- servers/fish-speech/run_server.py
+++ servers/fish-speech/run_server.py
@@ -4,17 +4,16 @@ from pathlib import Path
 from huggingface_hub import snapshot_download
 
 # Global cache directory
-CACHE_DIR = Path("/virtual/my_tmp/fish/checkpoints")
+CACHE_DIR = Path("checkpoints")
 MODEL_NAME = "fishaudio/s2-pro"
 CHECKPOINT_PATH = CACHE_DIR / "s2-pro"
 
 def download_model():
-    print("here")
     if not CHECKPOINT_PATH.exists():
         print(f"Downloading model {MODEL_NAME} to {CHECKPOINT_PATH}...")
         snapshot_download(
             repo_id=MODEL_NAME,
-            local_dir=CHECKPOINT_PATH,
+            local_dir=str(CHECKPOINT_PATH),
             local_dir_use_symlinks=False
         )
     else:
