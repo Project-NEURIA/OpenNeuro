483fabb1f17db90f190e4d39e6ad3ea0394ba779
Barryyj <barryy.jiang@mail.utoronto.ca>
2026-03-22 05:41:28 -0400

    Fish Audio Support (#68)
    * fishtts compatible local server, tts_fish node
    
    * delete unwanted files
    
    * change the CACHE_DIR in run_server i forgot I was using a strange local path
    
    * oops forgot to add bitsandbytes dep in .toml, hopefully the only one i forgot
    
    * feat: fix api call
    
    * moving server code to a seperate repo
    
    ---------
    
    Co-authored-by: xingjianll <4396kevinliu@gmail.com>

--- backend/src/core/conduit/__init__.py
+++ backend/src/core/conduit/__init__.py
@@ -5,6 +5,7 @@ from src.core.conduit.agent_state import AgentState as AgentState
 from src.core.conduit.character_card import CharacterCard as CharacterCard
 from src.core.conduit.llm import LLM as LLM
 from src.core.conduit.tts import TTS as TTS
+from src.core.conduit.tts_fish import FishTTS as FishTTS
 from src.core.conduit.discord import DiscordIO as DiscordIO
 from src.core.conduit.dart_control import DartControl as DartControl
 from src.core.conduit.object_detection_visualizer import (
