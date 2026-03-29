530d9768633af4ff27adea84a22831b6d682014f
MarksonChen <chenmarkson@gmail.com>
2026-03-28 23:01:32 -0400

    minor fixes

--- backend/src/core/conduit/ffs_stereo_depth/stereo_depth_estimator.py
+++ backend/src/core/conduit/ffs_stereo_depth/stereo_depth_estimator.py
@@ -134,7 +134,7 @@ class StereoDepthEstimator(
 
         with (
             torch.no_grad(),
-            torch.amp.autocast("cuda", enabled=True, dtype=torch.float16),
+            torch.amp.autocast(self._device.type, enabled=self._device.type != "cpu", dtype=torch.float16),
         ):
             disp = self._model.forward(
                 t0_pad,
